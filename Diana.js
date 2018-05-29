37431ce027849f9d121454725b4ff31de2c83f09
CircusRockS <37488996+CircusRockS@users.noreply.github.com>
2018-05-29 15:46:19 -0400

    Update Diana.js

--- Diana.js
+++ Diana.js
@@ -306,7 +306,7 @@ client.on("message", (message) => {
     if (!args) return message.channel.send("Escribe un titulo para buscarlo `&yt Circus Monster Mayu`");
     var opts = {
       maxResults: 1,
-      key: ' AIzaSyCmzFLInzqBuyVglFGq-dYTmKr77kvic_A '
+      key: ' API_KEY '
     };
 		youtube(args, opts, function(err, results) {
     if(err) return console.log(err);
